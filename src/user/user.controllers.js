dbfc775c581c6ce2eb04a149ee56489d5a372d69
Jamil Safdar <jamil.safdar@ao.com>
2021-11-16 10:14:06 +0000

    included routes model and method

--- src/user/user.controllers.js
+++ src/user/user.controllers.js
@@ -2,13 +2,13 @@ const User = require('./user.model');
 
 exports.addUser = async (req, res) => {
     try {
-      console.log(req);
+      // console.log(req.body);
 
-    //   const newUser = new User(req.body);
-          // await newUser.save();
+      const newUser = new User(req.body);
+          await newUser.save();
     res.status(200).res.send({message: 'success'});  
     } catch (error) {
         console.log(error)
-        res.status(500).send({message: 'Something went wrong, check server logs'};)
+        res.status(418).send({message: 'Something went wrong, check server logs'});
     }
 };
