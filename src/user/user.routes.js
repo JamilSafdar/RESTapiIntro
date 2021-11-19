e5b804cfa73922c44f6ad70b2ce9ce8483fe85af
Jamil Safdar <jamil.safdar@ao.com>
2021-11-19 14:02:40 +0000

    CRUD Update func added

--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { addUser, login, findUser, deleteUser } = require('./user.controllers');
+const { addUser, login, findUser, deleteUser, updateUser } = require('./user.controllers');
 const { hashPassword, comparePasswords, tokenAuth } = require('../middleware')
 const userRouter = Router();
 
@@ -8,7 +8,7 @@ userRouter.post('/user', /*Middleware goes here*/hashPassword, addUser)
 userRouter.post('/login', comparePasswords, login)
 userRouter.get('/token', tokenAuth, login)
 userRouter.post('/user/login', comparePasswords, findUser);
-userRouter.delete('/user/:username', deleteUser);
-
+userRouter.delete('/user/username', deleteUser);
+userRouter.put('/user/update', comparePasswords, updateUser);
 
 module.exports = userRouter;
